777aeca8467f6914c922af597f9bd30866c3cfb1
mhneri <mneri@krispsystems.com>
2019-01-22 23:33:49 +0800

    initial commit

---
+++ test/index.spec.ts
@@ -0,0 +1,11 @@
+import "mocha";
+import { expect } from "chai";
+
+import { sum } from "../src/index";
+
+describe("suite", () => {
+  it("sum should add", () => {
+    const result = sum(1, 2);
+    expect(result).to.equal(4);
+  });
+})
